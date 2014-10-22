12f88c4f6eafc88e47068816e2eb096262416e15
Carlos Gimeno Yáñez <cgimeno@bifi.es>
2014-10-22 13:58:02 +0200

    Initial commit

---
+++ glancepush.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+###############################################################################
+# AUTHOR: Carlos Gimeno                                                       #
+# EMAIL: cgimeno@bifi.es                                                      #
+# VERSION: 0.0.1                                                              #
+# DESCRIPTION: Software to upload images fetched from a VO image list         #
+#              using vmcatcher, to Openstack, using Openstack API             #
+###############################################################################
+
+import os
+import sys
+import tarfile
+import stat
+import argparse
+
+__author__ = "Carlos Gimeno"
+__license__ = "MIT"
+__maintainer__ = "Carlos Gimeno"
+__email__ = "cgimeno@bifi.es"
+__status__ = "Development"
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
