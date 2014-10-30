2841d7cc0326b1338e733e3acd57a25e22639bfe
Carlos Gimeno Yáñez <carlos.gimeno@protonmail.ch>
2014-10-30 16:57:51 +0100

    Moved the VO check at the beginning of publish.py for a better performance.

--- lib/publish.py
+++ lib/publish.py
@@ -41,63 +41,66 @@ def publish_image(image_file, image_name, image_format, container_format, is_pub
     if is_public == "\"yes\"":
         public = True
     else:
-        print "Is not public"
         public = False
 
     if is_protected == "\"yes\"":
         protect_image = True
     else:
         protect_image = False
-
-    json_file = open("/etc/glancepush/voms.json").read()
-    json_data = json.loads(json_file)
-    credentials = get_keystone_creds()
-    nova_credentials = get_nova_creds()
-    nova = nvclient.Client(insecure=is_secure, **nova_credentials)
-    keystone = ksclient.Client(insecure=is_secure, **credentials)
-    glance_endpoint = keystone.service_catalog.url_for(service_type='image', endpoint_type='publicURL')
-    glance = glanceclient.Client('1', glance_endpoint, token=keystone.auth_token)
-    # Check if image has been already uploaded.
-    found = False
-    upload = True
-    print "Processing " + image_name
-    try:
-        image = nova.images.find(name=image_name)
-        found = True
-    except novaclient.exceptions.NotFound:
-        pass
-    # If image exists, get his metadata
-    if found:
-        metadata = nova.images.get(image.id).metadata
-        old_version = metadata['vmcatcher_event_hv_version']
-        new_version = properties_dict['VMCATCHER_EVENT_HV_VERSION']
-        VO = properties_dict['VMCATCHER_EVENT_VO']
-        if old_version == new_version:
-            print "Same version: " + new_version
-            print "Skipping..."
-            print json_data[VO]["tenant"]
-            upload = False
-        elif VO != environ['OS_TENANT_NAME']:
-            print "This image is not assigned to this VO"
-            print "Skipping image"
-            upload = False
+    # Before doing anything else, check if this image is associated with current VO
+    VO = properties_dict['VMCATCHER_EVENT_VO']
+    if VO != environ['OS_TENANT_NAME'] :
+        print "This image is not associated with this VO " + image_name
+        print "Skipping..."
+    else:
+        # Process image
+        json_file = open("/etc/glancepush/voms.json").read()
+        json_data = json.loads(json_file)
+        credentials = get_keystone_creds()
+        nova_credentials = get_nova_creds()
+        nova = nvclient.Client(insecure=is_secure, **nova_credentials)
+        keystone = ksclient.Client(insecure=is_secure, **credentials)
+        glance_endpoint = keystone.service_catalog.url_for(service_type='image', endpoint_type='publicURL')
+        glance = glanceclient.Client('1', glance_endpoint, token=keystone.auth_token)
+        found = False
+        upload = True
+        print "Processing " + image_name
+        try:
+            # Check if image has been already uploaded.
+            image = nova.images.find(name=image_name)
+            found = True
+        except novaclient.exceptions.NotFound:
+            pass
+        # If image exists, get his metadata
+        if found:
+            metadata = nova.images.get(image.id).metadata
+            old_version = metadata['vmcatcher_event_hv_version']
+            new_version = properties_dict['VMCATCHER_EVENT_HV_VERSION']
+            VO = properties_dict['VMCATCHER_EVENT_VO']
+            if old_version == new_version:
+                print "Same version: " + new_version
+                print "Skipping..."
+                print json_data[VO]["tenant"]
+                upload = False
+            else:
+                try:
+                    print "New version found: " + new_version
+                    print "Uploading to tenant: " + json_data[VO]["tenant"]
+                    print "Deleting previous version..."
+                    delete = delete_image(image_name)
+                    print "Uploading new version"
+                except KeyError:
+                    print "ERROR! VO not defined in voms.json"
         else:
-            try:
-                print "New version found: " + new_version
-                print "Uploading to tenant: " + json_data[VO]["tenant"]
-                print "Deleting previous version..."
-                delete = delete_image(image_name)
-                print "Uploading new version"
-            except KeyError:
-                print "ERROR! VO not defined in voms.json"
-    if upload:
-        with open(image_file, 'r') as fimage:
-            image = glance.images.create(name=image_name, disk_format="\"" + image_format + "\"",
-                                         container_format="\"" + container_format + "\"",
-                                         data=fimage, properties=properties_dict, public=is_public,
-                                         is_protected=protect_image)
-    if upload:
-        print nova.images.get(image.id).metadata
+            print "Uploading to tenant: " + json_data[VO]["tenant"]
+        if upload:
+            with open(image_file, 'r') as fimage:
+                image = glance.images.create(name=image_name, disk_format="\"" + image_format + "\"",
+                                             container_format="\"" + container_format + "\"",
+                                             data=fimage, properties=properties_dict, public=is_public,
+                                             is_protected=protect_image)
+        if upload:
+            print nova.images.get(image.id).metadata
 
 
 def main():
